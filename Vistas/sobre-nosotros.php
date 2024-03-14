8a9389aa5cdab00ce6ff919be485dcc00327a822
agluee123 <bastosagustin1998@gmail.com>
2024-03-14 16:45:35 -0300

    se agrego icono de pestaña

--- Vistas/sobre-nosotros.php
+++ Vistas/sobre-nosotros.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Il gabinetto</title>
+    <link rel="shortcut icon" href="/ilgabinetto/imagenes/icon.png" type="image/x-icon">
     <!-- CSS Bootstrap -->
     <link rel="stylesheet" href="/ilgabinetto/css/bootstrap.min.css" />
     <link rel="stylesheet" href="/ilgabinetto/Vistas/style.css" />
